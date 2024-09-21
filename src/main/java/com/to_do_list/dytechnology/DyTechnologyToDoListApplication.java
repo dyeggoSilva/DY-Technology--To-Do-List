970bef69df80e96e0291aab0e19619191b5f3755
dyeggoSilva <dyeggo0102@live.com>
2024-09-21 15:41:10 -0300

    :tada: init: Commit inicial

---
+++ src/main/java/com/to_do_list/dytechnology/DyTechnologyToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.to_do_list.dytechnology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DyTechnologyToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DyTechnologyToDoListApplication.class, args);
+	}
+
+}
