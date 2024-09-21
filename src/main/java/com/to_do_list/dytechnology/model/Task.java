fadfda6211cf05d1773bf21bfcc31192d1e3d30d
dyeggoSilva <dyeggo0102@live.com>
2024-09-21 16:09:31 -0300

    :sparkles: feat: Create task.java

---
+++ src/main/java/com/to_do_list/dytechnology/model/Task.java
@@ -0,0 +1,27 @@
+package com.to_do_list.dytechnology.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private boolean done;
+    private  int priority;
+
+}
