218495e9c959ce4aeee70d37c5c7b12def83bd18
dyeggoSilva <dyeggo0102@live.com>
2024-09-23 20:50:11 -0300

    feat: lógica do service

--- src/main/java/com/to_do_list/dytechnology/controllers/TaskController.java
+++ src/main/java/com/to_do_list/dytechnology/controllers/TaskController.java
@@ -1,21 +1,40 @@
 package com.to_do_list.dytechnology.controllers;
 
 import com.to_do_list.dytechnology.dto.TaskDto;
+import com.to_do_list.dytechnology.model.Task;
 import com.to_do_list.dytechnology.services.TaskService;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TaskController {
     @Autowired
     private TaskService taskService;
+    @GetMapping("/tasks")
+    public List allTasks(){
+        return taskService.searchTasks();
+    }
+    @GetMapping("/task/{id}")
+    public Optional tasksById(@PathVariable Long id){
+        return taskService.searchTasksById(id);
+    }
 
-    @PostMapping("register-task")
+
+    @PostMapping("/register-task")
     public Object registerTask(@RequestBody TaskDto dataTask){
         return ResponseEntity.ok(taskService.creatTasks(dataTask));
-
+    }
+    @PutMapping("/update-task/{id}")
+    public Object updateTask(@PathVariable Long id, @RequestBody TaskDto dataTask){
+        return ResponseEntity.ok(taskService.updateTasksById(id,dataTask));
+    }
+    @DeleteMapping("/delete-task/{id}")
+    public Object deleterTask(@PathVariable Long id){
+        return ResponseEntity.ok(taskService.deleteTasks(id));
     }
 }
--- src/main/java/com/to_do_list/dytechnology/services/TaskService.java
+++ src/main/java/com/to_do_list/dytechnology/services/TaskService.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class TaskService {
     @Autowired
@@ -25,8 +28,42 @@ public class TaskService {
         return"Task created! (Tarefa Criada!)";
     }
 
-    public String searchTasks(){return "";}
-    public String searchTasksById(){return "";}
-    public String updateTasks(){return "";}
-    public String deleteTasks(){return "";}
+    public List<Task> searchTasks(){
+        return taskRepository.findAll();
+    }
+    public Optional<Task> searchTasksById(Long id){
+        return taskRepository.findById(id);
+    }
+    public String updateTasksById(long id, TaskDto dataTask){
+
+        Optional<Task> task = taskRepository.findById(id);
+
+        if(task.isPresent()){
+            Task updateTask = task.get();
+            if(dataTask.name()!= null){
+                updateTask.setName(dataTask.name());
+            }if(dataTask.done() == true){
+                updateTask.setDone(dataTask.done());
+            }if(dataTask.description() != null){
+                updateTask.setDescription(dataTask.description());
+            }if(dataTask.priority() != 0){
+                updateTask.setPriority(dataTask.priority());
+            }
+            taskRepository.save(updateTask);
+            return "dados alterados!";
+        }else { return "Task não encontrada!";}
+    }
+    public String deleteTasks(long id) {
+        Task deleteTask = new Task();
+
+        Optional task = taskRepository.findById(id);
+
+        if (task.isPresent()) {
+            taskRepository.deleteById(id);
+            return "Task deletada!";
+        } else {
+            return "Task não encontrada!";
+        }
+
+    }
 }
